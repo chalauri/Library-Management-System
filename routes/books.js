34eb61f5fa31b3bf8c06b57634c838fabb165cf1
Giga Chalauri <g.chalauri@mia.gov.ge>
2016-05-03 18:52:44 +0400

    add rating

--- routes/books.js
+++ routes/books.js
@@ -5,7 +5,7 @@ var express = require('express');
 var router = express.Router();
 
 /*Constructor Of Book*/
-function Book(title,author,isbn,year,comment,room,shelf) {
+function Book(title, author, isbn, year, comment, room, shelf) {
     this.title = title;
     this.author = author;
     this.isbn = isbn;
@@ -14,12 +14,13 @@ function Book(title,author,isbn,year,comment,room,shelf) {
     this.room = room;
     this.shelf = shelf;
     this.rating = [];
+    this.currRating = 0;
 }
 
 var books = [];
-var nodeByExamples = new Book('Node.js By Example','Krasimir Tsonev','978-1-78439-571-1',2015,'',1,1);
-var spring = new Book('Spring In Action 4th', 'Craig Walls','9781617291203',2015,'',1,2 );
-var beginningNodeJs = new Book('Beginning Node.Js', 'Basarat Ali Syed',2015,'','',1,3);
+var nodeByExamples = new Book('Node.js By Example', 'Krasimir Tsonev', '978-1-78439-571-1', 2015, '', 1, 1);
+var spring = new Book('Spring In Action 4th', 'Craig Walls', '9781617291203', 2015, '', 1, 2);
+var beginningNodeJs = new Book('Beginning Node.Js', 'Basarat Ali Syed', 2015, '', '', 1, 3);
 
 books.push(nodeByExamples, spring, beginningNodeJs);
 
@@ -35,8 +36,14 @@ router.post('/add', function (req, res, next) {
     var tempRoom = tempBook.room;
     var tempShelf = tempBook.shelf;
 
-    if(!checkPlace(tempRoom,tempShelf)){
+    if (!checkPlace(tempRoom, tempShelf)) {
         res.send("Place Is Not Available");
+        return;
+    }
+
+    var tempISBN = tempBook.isbn;
+    if (!checkISBN(tempISBN)) {
+        res.send("ISBN IS NOT UNIQUE");
         return;
     }
 
@@ -44,16 +51,71 @@ router.post('/add', function (req, res, next) {
     res.send(books);
 });
 
-function checkPlace(tempRoom,tempShelf){
+
+router.post('/addRating', function (req, res, next) {
+    var isbn = req.body.book.isbn;
+    var grade = req.body.grade;
+
+    var book = getBookByISBN(isbn);
+
+    if(grade < 0 || grade > 10){
+        res.send("ILLEGAL GRADE");
+        return;
+    }
+
+    if(book == null){
+        res.send("ILLEGAL BOOK");
+        return;
+    }
+
+    rating = book.rating;
+    rating.push(grade);
+    var currRating = 0;
+    var sum = 0;
+    for(var i = 0; i < rating.length; i++) {
+        sum += rating[i];
+    }
+    currRating = sum / rating.length;
+    book.currRating = currRating;
+
+    res.send(books);
+});
+
+
+function checkPlace(tempRoom, tempShelf) {
 
     var result = true;
     books.forEach(function (item) {
-        if(item.room == tempRoom && item.shelf == tempShelf ) {
-            result =  false;
+        if (item.room == tempRoom && item.shelf == tempShelf) {
+            result = false;
         }
     });
 
     return result;
 }
+
+function checkISBN(tempISBN) {
+
+    var result = true;
+    books.forEach(function (item) {
+        if (item.isbn == tempISBN) {
+            result = false;
+        }
+    });
+
+    return result;
+}
+
+function getBookByISBN(tempISBN) {
+
+    for(var i = 0; i < books.length; i++){
+        if(books[i].isbn == tempISBN){
+            return books[i];
+        }
+    }
+
+    return null;
+}
+
 
 module.exports = router;
