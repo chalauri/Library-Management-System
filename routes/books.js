f0e7f384666ad44d49f735e6c65dbb6fccf71398
Giga Chalauri <g.chalauri@mia.gov.ge>
2016-05-04 11:27:20 +0400

    User Methods

--- routes/books.js
+++ routes/books.js
@@ -5,7 +5,7 @@ var express = require('express');
 var router = express.Router();
 
 /*Constructor Of Book*/
-function Book(title, author, isbn, year, comment, room, shelf) {
+function Book(title, author, isbn, year, comment, room, shelf, quantity, taken) {
     this.title = title;
     this.author = author;
     this.isbn = isbn;
@@ -15,12 +15,14 @@ function Book(title, author, isbn, year, comment, room, shelf) {
     this.shelf = shelf;
     this.rating = [];
     this.currRating = 0;
+    this.quantity = quantity;
+    this.taken = taken;
 }
 
 var books = [];
-var nodeByExamples = new Book('Node.js By Example', 'Krasimir Tsonev', '978-1-78439-571-1', 2015, '', 1, 1);
-var spring = new Book('Spring In Action 4th', 'Craig Walls', '9781617291203', 2015, '', 1, 2);
-var beginningNodeJs = new Book('Beginning Node.Js', 'Basarat Ali Syed', 2015, '', '', 1, 3);
+var nodeByExamples = new Book('Node.js By Example', 'Krasimir Tsonev', '978-1-78439-571-1', 2015, '', 1, 1, 10, 0);
+var spring = new Book('Spring In Action 4th', 'Craig Walls', '9781617291203', 2015, '', 1, 2, 10, 0);
+var beginningNodeJs = new Book('Beginning Node.Js', 'Basarat Ali Syed', 2015, '', '', 1, 3, 10, 0);
 
 books.push(nodeByExamples, spring, beginningNodeJs);
 
@@ -35,15 +37,25 @@ router.post('/add', function (req, res, next) {
     var tempBook = req.body;
     var tempRoom = tempBook.room;
     var tempShelf = tempBook.shelf;
+    var quantity = tempBook.quantity;
+    tempBook.taken = 0;
+
+    if (quantity <= 0) {
+        res.writeHead(400, "Books quantity in library must be more than 0", {'content-type': 'application/json'});
+        res.end("Books quantity in library must be more than 0");
+        return;
+    }
 
     if (!checkPlace(tempRoom, tempShelf)) {
-        res.send("Place Is Not Available");
+        res.writeHead(400, "Place Is Not Available", {'content-type': 'application/json'});
+        res.end("Place Is Not Available");
         return;
     }
 
     var tempISBN = tempBook.isbn;
     if (!checkISBN(tempISBN)) {
-        res.send("ISBN IS NOT UNIQUE");
+        res.writeHead(400, "ISBN IS NOT UNIQUE", {'content-type': 'application/json'});
+        res.end("ISBN IS NOT UNIQUE");
         return;
     }
 
@@ -58,14 +70,14 @@ router.post('/addRating', function (req, res, next) {
 
     var book = getBookByISBN(isbn);
 
-    if(grade < 0 || grade > 10){
-        res.writeHead(400, "ILLEGAL GRADE", {'content-type' : 'application/json'});
+    if (grade < 0 || grade > 10) {
+        res.writeHead(400, "ILLEGAL GRADE", {'content-type': 'application/json'});
         res.end("ILLEGAL GRADE");
         return;
     }
 
-    if(book == null){
-        res.writeHead(400, "ILLEGAL BOOK", {'content-type' : 'application/json'});
+    if (book == null) {
+        res.writeHead(400, "ILLEGAL BOOK", {'content-type': 'application/json'});
         res.end("ILLEGAL BOOK");
         return;
     }
@@ -74,7 +86,7 @@ router.post('/addRating', function (req, res, next) {
     rating.push(grade);
     var currRating = 0;
     var sum = 0;
-    for(var i = 0; i < rating.length; i++) {
+    for (var i = 0; i < rating.length; i++) {
         sum += rating[i];
     }
     currRating = sum / rating.length;
@@ -110,8 +122,8 @@ function checkISBN(tempISBN) {
 
 function getBookByISBN(tempISBN) {
 
-    for(var i = 0; i < books.length; i++){
-        if(books[i].isbn == tempISBN){
+    for (var i = 0; i < books.length; i++) {
+        if (books[i].isbn == tempISBN) {
             return books[i];
         }
     }
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,50 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+function User(name, surname, personalNo, phone, mail, address, username, password) {
+    this.name = name;
+    this.surname = surname;
+    this.personalNo = personalNo;
+    this.phone = phone;
+    this.mail = mail;
+    this.address = address;
+    this.username = username;
+    this.password = password;
+}
+
+var users = [];
+var chalauri = new User("Giga", "Chalauri", "01234567891", "591115008", "giga.chalauri@gmail.com", "Unknown", "chalauri", "chalauri");
+users.push(chalauri);
+
+router.get('/list', function (req, res, next) {
+    res.send(users);
 });
+
+router.post('/add', function (req, res, next) {
+    var tempUser = req.body;
+    var personalNo = tempUser.personalNo;
+
+    var tempISBN = tempBook.isbn;
+    if (!checkUser(personalNo)) {
+        res.writeHead(400, "User already exists", {'content-type': 'application/json'});
+        res.end("User already exists");
+        return;
+    }
+
+    books.push(tempBook);
+    res.send(books);
+});
+
+function checkUser(tempPersonalNo) {
+    var result = true;
+    books.forEach(function (item) {
+        if (item.personalNo == tempPersonalNo) {
+            result = false;
+        }
+    });
+
+    return result;
+}
+
 
 module.exports = router;
